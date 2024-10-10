907c6cd604d4950e6cc6c4f5fd040bbafdc89fd8
jwj51720 <jwj51720@gmail.com>
2024-10-10 22:28:19 +0900

    S1

--- Problem/Challenging/18290.py
+++ Problem/Challenging/18290.py
@@ -7,20 +7,22 @@ def is_valid(solution, v):
     return True
 
 
-def backtracking(solution, MATRIX, K, current_sum, results):
+def backtracking(solution, MATRIX, K, results, node, visited):
     if len(solution) == K:
-        results.append(current_sum)
+        ans = 0
+        for sol in solution:
+            ans += MATRIX[sol[0]][sol[1]]
+        results.append(ans)
         return
 
-    for row in range(len(MATRIX)):
-        for col in range(len(MATRIX[0])):
-            if is_valid(solution, [row, col]):
+    for row in range(node[0], len(MATRIX)):
+        for col in range(node[1] if row == node[0] else 0, len(MATRIX[0])):
+            if is_valid(solution, [row, col]) and not visited[row][col]:
                 solution.append([row, col])
-                current_sum += MATRIX[row][col]
-                backtracking(solution, MATRIX, K, current_sum, results)
-                current_sum -= MATRIX[row][col]
+                visited[row][col] = True
+                backtracking(solution, MATRIX, K, results, [row, col], visited)
+                visited[row][col] = False
                 solution.pop()
-            break
 
 
 def main(MATRIX, N, M, K):
@@ -36,8 +38,9 @@ def main(MATRIX, N, M, K):
     선택했다가 되돌리는 방식은? -> 백트래킹
     """
     results = []
-    backtracking([], MATRIX, K, 0, results)
-    print((results))
+    visited = [[False] * M for _ in range(N)]
+    backtracking([], MATRIX, K, results, [0, 0], visited)
+    print(max(results))
     return
 
 
@@ -45,3 +48,9 @@ if __name__ == "__main__":
     N, M, K = map(int, input().split())
     MATRIX = [list(map(int, input().split())) for _ in range(N)]
     main(MATRIX, N, M, K)
+
+"""
+신경쓰지 못했던 지점: 백트래킹은 낭비하는 탐색이 없어야 함
+- visited 행렬 만들지 않은 것 -> 그래프 형태라면 원래 방문했던 곳을 다시 검토하는 게 의미 없음
+- 탐색 시작 노드의 아래 부분, 오른쪽 부분만 탐색하면 된다는 것
+"""
